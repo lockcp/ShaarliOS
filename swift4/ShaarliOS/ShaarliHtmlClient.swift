3917dd14adb0f7c00559a52064f01812c47cb5c5
Marcus Rohrmoser <work@mro.name>
2019-06-20 20:37:53 +0200

    fix

--- swift4/ShaarliOS/ShaarliHtmlClient.swift
+++ swift4/ShaarliOS/ShaarliHtmlClient.swift
@@ -72,32 +72,34 @@ class ShaarliHtmlClient {
         var req0 = URLRequest(url:uc.url!)
         req0.setValue(VAL_HEAD_USER_AGENT, forHTTPHeaderField:KEY_HEAD_USER_AGENT)
 
-        let check: (Data?, URLResponse?, Error?, String) -> ([String:String],String) = {
+        let check: (Data?, URLResponse?, Error?) -> String = {
             if let error = $2 {
-                return ([:], error.localizedDescription)
+                return error.localizedDescription
             }
             guard let http = $1 as? HTTPURLResponse else {
-                return ([:], String(format:"Not a http reponse, but %@", $1 ?? "<nil>"))
+                return String(format:"Not a http reponse, but %@", $1 ?? "<nil>")
             }
             if !(200...299).contains(http.statusCode) {
-                return ([:], String(format:"Expected status 200, got %d", http.statusCode))
-            }
-            guard let fo = findForms($0, http.textEncodingName)[$3] else {
-                return ([:], $3 + " not found")
+                return String(format:"Expected status 200, got %d", http.statusCode)
             }
 
-            return (fo, "")
+            return ""
         }
         
         // https://developer.apple.com/documentation/foundation/url_loading_system/fetching_website_data_into_memory
         let t0 = ses.dataTask(with: req0) { data, response, erro in
-            let ret = check(data, response, erro, ShaarliHtmlClient.LOGIN_FORM)
-            if ret.1 != "" {
-                completion(URLEmpty, "", ret.1)
+            let err = check(data, response, erro)
+            if err != "" {
+                completion(URLEmpty, "", err)
                 return
             }
             let http = response as! HTTPURLResponse
-            var lofo = ret.0
+
+            guard var lofo = findForms(data, http.textEncodingName)[ShaarliHtmlClient.LOGIN_FORM]
+            else {
+                completion(http.url!, "", ShaarliHtmlClient.LOGIN_FORM + " not found")
+                return
+            }
             lofo[ShaarliHtmlClient.KEY_FORM_LOGIN] = endpoint.user
             lofo[ShaarliHtmlClient.KEY_FORM_PASSWORD] = endpoint.password
 
@@ -107,30 +109,30 @@ class ShaarliHtmlClient {
             req1.setValue(VAL_HEAD_CONTENT_TYPE, forHTTPHeaderField:KEY_HEAD_CONTENT_TYPE)
             let formDat = formData(lofo)
             let t1 = ses.uploadTask(with: req1, from: formDat) { data, response, erro in
-                let ret = check(data, response, erro, ShaarliHtmlClient.LINK_FORM)
-                if ret.1 != "" {
-                    completion(URLEmpty, "", ret.1)
+                let err = check(data, response, erro)
+                if err != "" {
+                    completion(URLEmpty, "", err)
                     return
                 }
                 let http = response as! HTTPURLResponse
-                let lifo = ret.0
-                if lifo == [:] {
-                    let enco = encoding(name:http.textEncodingName)
-                    let str = String(bytes: data!, encoding:enco) ?? ""
-                    if let ra = str.range(of: ShaarliHtmlClient.PAT_WRONG_LOGIN, options:.regularExpression) {
-                        let err = String(str[ra]).dropFirst(15).dropLast(3)
-                        completion(URLEmpty, "", String(err))
-                        return
-                    }
-                    if ShaarliHtmlClient.STR_BANNED == str {
-                        completion(URLEmpty, "", ShaarliHtmlClient.STR_BANNED)
-                        return
-                    }
-
-                    completion(URLEmpty, "", ShaarliHtmlClient.LINK_FORM + " not found.")
-                    return
-                }
                 let _ = http.mimeType ?? ""
+                guard let lifo = findForms(data, http.textEncodingName)[ShaarliHtmlClient.LINK_FORM]
+                    else {
+                        let enco = encoding(name:http.textEncodingName)
+                        let str = String(bytes: data!, encoding:enco) ?? ""
+                        if let ra = str.range(of: ShaarliHtmlClient.PAT_WRONG_LOGIN, options:.regularExpression) {
+                            let err = String(str[ra]).dropFirst(15).dropLast(3)
+                            completion(URLEmpty, "", String(err))
+                            return
+                        }
+                        if ShaarliHtmlClient.STR_BANNED == str {
+                            completion(URLEmpty, "", ShaarliHtmlClient.STR_BANNED)
+                            return
+                        }
+
+                        completion(URLEmpty, "", ShaarliHtmlClient.LINK_FORM + " not found.")
+                        return
+                }
 
                 if nil == lifo[ShaarliHtmlClient.LF_URL] {
                     completion(URLEmpty, "", ShaarliHtmlClient.LF_URL + " not found.")
