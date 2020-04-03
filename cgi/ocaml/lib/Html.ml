9033a0d2d943463038b443c8a131f218e5c00449
Marcus Rohrmoser <work@mro.name>
2020-04-03 10:58:12 +0200

    clean

--- cgi/ocaml/lib/Html.ml
+++
@@ -1,6 +0,0 @@
-(*
- * https://code.mro.name/mro/ShaarliOS/src/4481a7e7d30c36d50adaa064fe2dfe7202cd365a/swift4/ShaarliOS/HtmlFormParser.swift#L91
- *
- * Eigentlich brauchen wir die Formulare als https://dev.realworldocaml.org/records.html.
- *)
-
