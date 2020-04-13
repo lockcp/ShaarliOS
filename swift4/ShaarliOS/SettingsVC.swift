d45ff2d65cde7533818c56bbe2ac4409e88d8a26
Marcus Rohrmoser <work@mro.name>
2020-04-13 17:32:45 +0200

    Revert "Probe less."
    This reverts commit 8031683bc75932ad17e136592d18c02d1de9f93b.

--- swift4/ShaarliOS/SettingsVC.swift
+++ swift4/ShaarliOS/SettingsVC.swift
@@ -216,6 +216,12 @@ class SettingsVC: UITableViewController, UITextFieldDelegate, WKNavigationDelega
         ep.scheme = HTTP_HTTPS; urls.append(ep.url!)
         ep.scheme = HTTP_HTTP;  urls.append(ep.url!)
 
+        if ep.path.hasSuffix("/shaarli.cgi") {
+            ep.path = "\(ep.path.dropLast("/shaarli.cgi".count))/"
+        }
+        ep.scheme = HTTP_HTTPS; urls.append(ep.url!)
+        ep.scheme = HTTP_HTTP;  urls.append(ep.url!)
+
         return urls
     }
 
